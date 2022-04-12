764c617ecc996f29ca7f3c53bca320c91556b526
Yassine Zitoun <47726914+zwayten@users.noreply.github.com>
2022-04-12 04:48:14 +0200

    jghf

--- app/src/main/java/com/example/androidpim/view/ProfileUser.kt
+++ app/src/main/java/com/example/androidpim/view/ProfileUser.kt
@@ -2,6 +2,7 @@ package com.example.androidpim.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidpim.R
 
 class ProfileUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
