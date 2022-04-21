ddf246592023f587d4dcc6f3529a7b26906c9708
Yassine Zitoun <47726914+zwayten@users.noreply.github.com>
2022-04-21 05:39:45 +0200

    13

--- app/src/main/java/com/example/androidpim/adapters/learningAdapter.kt
+++ app/src/main/java/com/example/androidpim/adapters/learningAdapter.kt
@@ -5,16 +5,17 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidpim.R
 
 class learningAdapter {
-
+/*
 
     class favoriteViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
         val favoritepic = itemView.findViewById<ImageView>(R.id.champPic)
         val nom = itemView.findViewById<TextView>(R.id.Nom)
         val dispo = itemView.findViewById<TextView>(R.id.dispo)
         val prix = itemView.findViewById<TextView>(R.id.prix)
-        val btn_delete = itemView.findViewById<ImageButton>(R.id.btnLike)
+        val btn_delete = itemView.findViewById<ImageButton>(R.id.btnLike)*/
 
-    }
+    //}
 }
