115f458b63bd9a6b899a45cac4aed904b0dd3739
Yassine Zitoun <47726914+zwayten@users.noreply.github.com>
2022-04-21 18:17:58 +0200

    load images events

--- app/src/main/java/com/example/androidpim/adapters/EventAdapter.kt
+++ app/src/main/java/com/example/androidpim/adapters/EventAdapter.kt
@@ -1,27 +1,38 @@
 package com.example.androidpim.adapters
 
+import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.androidpim.R
 import com.example.androidpim.models.Event
 
 class EventAdapter (val eventList: List<Event>) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
 
 
-
+    var mContext: Context? = null
 
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.event_single, parent, false)
+        this.mContext = parent.getContext();
         return EventViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+
+
+        val ppp = "http://10.0.2.2:3000/upload/download/"+eventList[position].banner
+
+        this.mContext?.let { Glide.with(it).load(Uri.parse(ppp)).into(holder.eventImage) }
+
          holder.eventTitle.setText(eventList[position].title.toString())
 
          holder.eventPlace.setText(eventList[position].place.toString())
@@ -40,6 +51,7 @@ class EventAdapter (val eventList: List<Event>) : RecyclerView.Adapter<EventAdap
         val eventPlace = itemView.findViewById<TextView>(R.id.eventPlace)
         val eventTime = itemView.findViewById<TextView>(R.id.eventTime)
         val eventparticipants = itemView.findViewById<TextView>(R.id.eventparticipants)
+        val eventImage = itemView.findViewById<ImageView>(R.id.eventImage)
 
 
     }
--- app/src/main/java/com/example/androidpim/view/LkolPro.kt
+++ app/src/main/java/com/example/androidpim/view/LkolPro.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.androidpim.R
 import com.example.androidpim.fragments.EventFragment
@@ -62,9 +63,12 @@ class LkolPro : AppCompatActivity() {
         when (view.id) {
             R.id.home_icon -> {
                 getSupportFragmentManager().beginTransaction().replace(R.id.frame, HomePro()).commit()
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameEvent, EventFragment()).commit()
             }
             R.id.search_icon -> {
                 getSupportFragmentManager().beginTransaction().replace(R.id.frame, EventFragment()).commit()
+                getSupportFragmentManager().beginTransaction().remove(EventFragment()).commit()
+
             }
             /*
             R.id.add_icon -> {
@@ -77,6 +81,7 @@ class LkolPro : AppCompatActivity() {
 
             R.id.profile_icon -> {
                 getSupportFragmentManager().beginTransaction().replace(R.id.frame, ProfileUser()).commit()
+                getSupportFragmentManager().beginTransaction().remove(EventFragment()).commit()
             }
 
 
