20b2cbdf27a5ef3e22b93127429cfcc15c562d73
Yassine Zitoun <47726914+zwayten@users.noreply.github.com>
2022-04-21 11:47:09 +0200

    remove action bar

--- app/src/main/java/com/example/androidpim/view/LkolPro.kt
+++ app/src/main/java/com/example/androidpim/view/LkolPro.kt
@@ -20,6 +20,11 @@ class LkolPro : AppCompatActivity() {
     lateinit var  camera_img:ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
         setContentView(R.layout.activity_lkol_pro)
         supportActionBar?.hide();
 
